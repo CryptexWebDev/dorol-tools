c883c293a6cb15af03a2da7ab76ba0566f2d6ce4
Loki <you@example.com>
2024-10-24 00:36:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# DOROL Node Tools and scripts
+
+This repository contains the tools and scripts used to deploy and manage the DOROL full node.
+
+## Installation
+
+open a terminal and run the following commands:
+
+```bash
+/bin/bash -c "$(curl -fsSL curl /HEAD/install-node.sh)"
+```
+
+This will download and install all the necessary tools, software and scripts to run the DOROL full node and Validator.
---
+++ scripts/install-node.sh
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+cat <<EOS
+${tty_bold}Dorol Node Tools Installer v.0.1${tty_reset}
+
+EOS
+# apple intel/amd64 deposit cli
+DEPOSIT_CLI_MAC="https://github.com/CryptexWebDev/staking-deposit-cli/releases/download/2.7.0/deposit-cli-amd64-darwin.tgz"
+# linux intel/amd64 deposit cli
+DEPOSIT_CLI_LINUX="https://github.com/CryptexWebDev/staking-deposit-cli/releases/download/2.7.0/deposit-cli-amd64-linux.tgz"
+# TODO apple arm64 deposit cli
+# apple/mac geth amd64
+GETH_MAC_AMD64="https://github.com/CryptexWebDev/Dorol-Chain/releases/download/0.1.0/geth-darwin-amd64.tar.gz"
+# apple/mac geth arm64
+GETH_MAC_ARM64="https://github.com/CryptexWebDev/Dorol-Chain/releases/download/0.1.0/geth-darwin-arm64.tar.gz"
+# linux geth amd64
+GETH_LINUX_AMD64="https://github.com/CryptexWebDev/Dorol-Chain/releases/download/0.1.0/geth-linux-amd64.tar.gz"
+# linux geth arm64
+GETH_LINUX_ARM64="https://github.com/CryptexWebDev/Dorol-Chain/releases/download/0.1.0/geth-linux-arm64.tar.gz"
+# prysm beacon chain mac
+PRYSM_BEACON_CHAIN_MAC_AMD64="https://github.com/prysmaticlabs/prysm/releases/download/v5.1.2/beacon-chain-v5.1.2-darwin-amd64"
+# prysm beacon chain linux
+PRYSM_BEACON_CHAIN_LINUX=""
+
+NODE_OS="unknown"
+NODE_ARCH="unknown"
+NODE_DATA_DIR="$HOME/.drlnet"
+NODE_BIN_DIR="$HOME/dorol/bin"
+NODE_SCRIPTS_DIR="$HOME/dorol/scripts"
+
+ARCH=$(uname -m)
+
+TMP_DIR=$(mktemp -d)
+
+cd $TMP_DIR
+
+if test -d "$NODE_BIN_DIR"; then
+    read -p "Node bin dir found. Do you wish to reinstall node software? " yn
+    case $yn in
+        [Yy]* ) rm -rf $NODE_BIN_DIR;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+fi
+
+if test -d "$NODE_SCRIPTS_DIR"; then
+    read -p "Node bin dir found. Do you wish to reinstall node management scripts? " yn
+    case $yn in
+        [Yy]* ) rm -rf $NODE_SCRIPTS_DIR;;
+        [Nn]* ) ;;
+        * ) echo "Please answer yes or no.";;
+    esac
+fi
+
+mkdir -p $NODE_BIN_DIR || echo "Failed to create node bin dir"
+mkdir -p $NODE_SCRIPTS_DIR || echo "Failed to create node scripts dir"
+
+
+if [[ "$OSTYPE" == "linux"* ]]; then
+        NODE_OS="linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        NODE_OS="darwin"
+else
+        echo "Unsupported OS: $OSTYPE/"
+        exit 1
+fi
+
+if [[ "$ARCH" == "x86_64" ]]; then
+       NODE_ARCH="amd64"
+elif [[ "$ARCH" == "arm64" ]]; then
+       NODE_ARCH="arm64"
+else
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+fi
+
+echo "Downloading node software..."
+GETH_DIST=""
+
+if [[ "$NODE_OS" == "linux" ]]; then
+        if [[ "$NODE_ARCH" == "amd64" ]]; then
+                GETH_DIST=$GETH_LINUX_AMD64
+        elif [[ "$NODE_ARCH" == "arm64" ]]; then
+                GETH_DIST=$GETH_LINUX_ARM64
+        fi
+elif [[ "$NODE_OS" == "darwin" ]]; then
+        if [[ "$NODE_ARCH" == "amd64" ]]; then
+                GETH_DIST=$GETH_MAC_AMD64
+        elif [[ "$NODE_ARCH" == "arm64" ]]; then
+                GETH_DIST=$GETH_MAC_ARM64
+        fi
+fi
+
+if [[ -z "$GETH_DIST" ]]; then
+        echo "Failed to determine Geth distribution"
+        exit 1
+fi
+
+echo "Downloading Geth from $GETH_DIST"
+curl -L -o geth.tar.gz $GETH_DIST
+
+DEPOSIT_CLI=""
+
+if [[ "$NODE_OS" == "linux" ]]; then
+        DEPOSIT_CLI=$DEPOSIT_CLI_LINUX
+elif [[ "$NODE_OS" == "darwin" ]]; then
+        DEPOSIT_CLI=$DEPOSIT_CLI_MAC
+fi
+
+if [[ -z "$DEPOSIT_CLI" ]]; then
+        echo "Failed to determine deposit CLI distribution"
+        exit 1
+fi
+
+echo "Downloading deposit CLI from $DEPOSIT_CLI"
+
+curl -L -o deposit-cli.tar.gz $DEPOSIT_CLI
+
+echo "Extracting Geth..."
+
+tar -xzf geth.tar.gz
+
+echo "Extracting deposit CLI..."
+
+tar -xzf deposit-cli.tar.gz
+
+echo "Install  binaries to $NODE_BIN_DIR..."
+
+mv geth $NODE_BIN_DIR
+mv deposit $NODE_BIN_DIR
+
+if [[ "$NODE_OS"=="darwin" ]]; then
+    echo "Removing quarantine attribute from binaries. You may be prompted for your password."
+    sudo -s xattr -d com.apple.quarantine NODE_BIN_DIR/*
+    echo "Removing quarantine attribute from scripts..."
+    sudo -s xattr -d com.apple.quarantine NODE_SCRIPTS_DIR/*
+fi
