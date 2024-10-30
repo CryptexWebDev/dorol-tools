ad09d2bbcaf5372a056a3d3cd4cf082dd9e0752d
Loki <you@example.com>
2024-10-31 01:08:47 +0200

    validator-prepare fix

--- scripts/hot-fix.sh
+++ scripts/hot-fix.sh
@@ -15,7 +15,7 @@ cd $TMP_DIR
 
 SCRIPTS="https://github.com/CryptexWebDev/dorol-tools/releases/download/0.1/scripts.tar.gz"
 DEPOSIT_SEND="https://github.com/CryptexWebDev/Deposit-Send/releases/download/0.0.1/deposit-send-linux-amd64.tar.gz"
-
+VALIDATOR_PREPARE="https://raw.githubusercontent.com/CryptexWebDev/dorol-tools/refs/heads/main/scripts/validator-prepare.sh"
 echo "
 $(tput setaf 3)$(tput bold)
 Dorol Node HotFix $(tput sgr0)
@@ -33,5 +33,10 @@ tar -xzf deposit-send.tar.gz
 
 mv deposit-send $NODE_BIN_DIR/deposit-send
 chmod +x $NODE_BIN_DIR/deposit-send
+
+cd $NODE_SCRIPTS_DIR
+
+curl -L -o validator-prepare.sh $VALIDATOR_PREPARE
+chmod +x validator-prepare.sh
 
 echo "Hot Fix Complete: $NODE_TOOLS_VERSION"
